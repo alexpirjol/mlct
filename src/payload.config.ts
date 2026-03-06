863512fad2c3adecf77d7aff7f3c0213f261d874
Alexandru Pirjol <alexandru.pirjol@softvision.com>
2026-03-06 15:56:12 +0200

    remvoe secure

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -68,7 +68,6 @@ export default buildConfig({
     transportOptions: {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 587,
-      secure: Number(process.env.SMTP_PORT) === 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
