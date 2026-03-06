245345cf54529d013c7942d70e993a3633830f58
Alexandru Pirjol <alexandru.pirjol@softvision.com>
2026-03-06 15:50:14 +0200

    add secure smtp port

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -68,6 +68,7 @@ export default buildConfig({
     transportOptions: {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 587,
+      secure: Number(process.env.SMTP_PORT) === 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
